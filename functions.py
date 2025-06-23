5e598a4d8b860bd91c42ffa4e3812f87550101b4
Dhiman K. Chakraborty <dhimankumarchakraborty@gmail.com>
2025-06-23 09:16:14 +0530

    Redid the basis set

--- functions.py
+++ functions.py
@@ -3,8 +3,6 @@ from itertools import combinations
 
 
 def basis_set_generator(tot_sites, N_e_up, N_e_down): # Checked OK
-    basis = []
-
     spin_up_basis = []
     spin_down_basis = []
 
@@ -19,13 +17,11 @@ def basis_set_generator(tot_sites, N_e_up, N_e_down): # Checked OK
         for idx in comb_down:
             state[idx] = 1
         spin_down_basis.append(state)
-    
-    spin_up_basis = np.array(spin_up_basis)
-    spin_down_basis = np.array(spin_down_basis)
+
+    basis = []
 
     for up_state in spin_up_basis:
         for down_state in spin_down_basis:
-            basis.append([up_state, down_state])
+            basis.append(up_state + down_state)
     
     return np.array(basis)
-
